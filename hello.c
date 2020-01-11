dafe0d10652b59785d3152b235869a5b611a8540
igram <igram.jp@gmail.com>
2020-01-11 10:13:51 +0900

    macro

--- hello.c
+++ hello.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+/*
+#define old_maxint(a,b) \
+    {int _a = (a), _b = (b); _a > _b ? _a : _b; }
+*/
+
+#define new_maxint(a,b) \
+    ({int _a = (a), _b = (b); _a > _b ? _a : _b; })
+
 int main(void)
 {
     printf("hello, world.\n");
+    
+    int n;
+    //n = old_maxint(1, 10);
+    n = new_maxint(1, 10);
+    printf("%d\n", n);
+
     return 0;
 }
